920055b8e731f5cd12bf1d98c1c28882a0c7a4ff
daman599 <damanpreetk117@gmail.com>
2026-02-23 23:33:43 +0530

    replaced id with key prop

--- components/shadows/shadow-grid.tsx
+++ components/shadows/shadow-grid.tsx
@@ -5,7 +5,7 @@ export const ShadowGrid = ({ shadows }: { shadows: Shadow[] }) => {
     return (
         <div className="flex items-start gap-8 lg:gap-12 flex-wrap px-1 md:px-3">
             {shadows.map((item) => (
-                <div id={item.id}>
+                <div key={item.id}>
                     <ShadowCard shadow={item} />
                 </div>
             ))}
