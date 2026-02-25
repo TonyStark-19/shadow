b62d5d71b068133ff2d8146f054a3dbf7c04dec3
daman599 <damanpreetk117@gmail.com>
2026-02-25 17:42:37 +0530

    analytics component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -40,6 +41,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
